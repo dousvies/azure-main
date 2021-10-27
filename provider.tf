facaa9127cde133838f377ab47e91d682e505ba5
Thiago Alonso <thralonso@gmail.com>
2021-10-27 17:45:14 +0100

    tests terraform authentication

--- provider.tf
+++ provider.tf
@@ -18,15 +18,7 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
   features {}
 }
 
-provider "azuread" {
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-}
+provider "azuread" {}
